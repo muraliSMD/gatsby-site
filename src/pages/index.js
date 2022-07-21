b2f65cfa3dc9fa979ef1fab7469d46120d749d3f
muraliSMD <muralis.1995@gmail.com>
2022-07-21 12:41:31 +0530

    replace

--- src/pages/index.js
+++ src/pages/index.js
@@ -84,7 +84,7 @@ const links = [
     text: "Tutorial",
     url: "https://www.gatsbyjs.com/docs/tutorial/",
     description:
-      "Designed to guide you through setting up your first Gatsby site.",
+      "good way to update welcome to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
     color: "#E95800",
   },
   {
